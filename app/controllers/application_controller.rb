46086642efa78613312e76f779e5ac740cf8153d
kuuleon <tudopassa427911@gmail.com>
2021-09-15 18:43:36 +0900

    herokuのデプロイ

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  #RailsアプリケーションにBasic認証を導入
+  before_action :basic_auth
+
+
+  private
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == 'admin' && password == '2222'
+    end
+  end
 end
